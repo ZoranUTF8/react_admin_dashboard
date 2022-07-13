eaed79835841bf826618df2e65dab96c7724fe1e
ZoranUTF8 <janjiczoran23@gmail.com>
2022-07-13 14:04:03 +0200

    Add delete user from table

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -5,22 +5,36 @@ import { DataGrid } from "@mui/x-data-grid";
 import { userColumns } from "../../data/datatablesource";
 import { userRows } from "../../data/datatablesource";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Datatable = () => {
+  const [data, setData] = useState(userRows);
+
+  //?Delete row
+
+  const handleDelete = (id) => {
+    console.log("hjere");
+    setData(data.filter((item) => item.id !== id));
+  };
+
   //* Action buttons in the table
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" className="linkComponent">
-              <div className="viewButton"> View </div>{" "}
+              <div className="viewButton"> View </div>
             </Link>
-            <Link to="/users/test" className="linkComponent">
-              <div className="deleteButton"> Delete </div>{" "}
-            </Link>{" "}
+
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
           </div>
         );
       },
@@ -31,12 +45,12 @@ const Datatable = () => {
     <div className="datatable">
       <div className="datatableTitle">
         <Link to="/users/new" className="linkComponent">
-          Add New User{" "}
-        </Link>{" "}
-      </div>{" "}
+          Add New User
+        </Link>
+      </div>
       <DataGrid
         className="dataGrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
